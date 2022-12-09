274c0dc58bf64a0759c786c2d5640f58237851fb
ywa422 <ywa422@sfu.ca>
2022-12-08 17:53:08 -0800

    alter run command

--- speed_ywa422/visualization/chord_vis.py
+++ speed_ywa422/visualization/chord_vis.py
@@ -1,9 +1,19 @@
 import numpy as np
 import matplotlib as mpl
+import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 import holoviews as hv
 from holoviews import opts, dim
+
+'''
+Run Command
+python3 chord_vis.py ../data
+
+Function
+This part of code read the commute frequncy of each Borough and generate a chord graph.
+Graph won't show in the Linux environment.
+'''
 
 hv.extension('bokeh')
 hv.output(size=200)
@@ -22,9 +32,14 @@ def map(str):
 	else:
 		return 5
 
-data = pd.read_csv('speed_ywa422/data/five_year_query_data/borough_speed/borough_speed.csv')
-data['source'] = data['PUborough'].map(lambda a : map(a))
-data['target'] = data['DOborough'].map(lambda a: map(a))
-link = data[['source', 'target', 'avg(speed)']]
-link = link.rename(columns={'avg(speed)': 'value'})
-hv.Chord(link)
+def main(input):
+	data = pd.read_csv(input + '/five_year_query_data/borough_speed/borough_speed.csv')
+	data['source'] = data['PUborough'].map(lambda a : map(a))
+	data['target'] = data['DOborough'].map(lambda a: map(a))
+	link = data[['source', 'target', 'avg(speed)']]
+	link = link.rename(columns={'avg(speed)': 'value'})
+	hv.Chord(link)
+
+if __name__ == '__main__':		
+	inputs = sys.argv[1]
+	main(inputs)
